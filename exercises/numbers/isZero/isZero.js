80062f1898c5fe80dd9000b245a6009a4d8547ba
shcooper <59896017+shaahcooper3@users.noreply.github.com>
2020-01-16 15:34:44 -0800

    Update isZero.js

--- exercises/numbers/isZero/isZero.js
+++ exercises/numbers/isZero/isZero.js
@@ -1,9 +1,3 @@
-/**
- * Returns true if the given number is 0 and false otherwise
- *
- * @param {number} num - The input number
- * @returns {boolean} True if num is 0 and false otherwise
- */
 function isZero(num) {
   return num === 0;
 }
